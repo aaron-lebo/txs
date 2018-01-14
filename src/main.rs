9e74adc668aee757a597e517028750c06c719c05
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2018-01-14 02:28:00 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+extern crate sha2;
+
+use sha2::{Digest, Sha256};
+use std::{mem, time};
+
+#[derive(Debug)]
+struct Block {
+    timestamp: u64,
+    data: Vec<u8>,
+    prev_hash: Vec<u8>,
+    hash: Vec<u8>,
+}
+
+impl Block {
+    fn new(data: &[u8], prev_hash: &[u8]) -> Self {
+        let ts = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap();
+        let ts = ts.as_secs() * 1000 + ts.subsec_nanos() as u64 / 1_000_000;
+        let hash = {
+            let mut hasher = Sha256::default();
+            let ts_bytes: [u8; 8] = unsafe { mem::transmute(ts.to_be()) };
+            hasher.input(&ts_bytes);
+            hasher.input(data);
+            hasher.input(prev_hash);
+            hasher.result()[..].to_owned()
+        };
+        Block {
+            timestamp: ts,
+            data: data.to_owned(),
+            prev_hash: prev_hash.to_owned(),
+            hash: hash,
+        }
+    }
+}
+
+struct Blockchain {
+    blocks: Vec<Block>,
+}
+
+impl Blockchain {
+    fn new() -> Self {
+        Blockchain {
+            blocks: vec![Block::new(b"genesis", b"")],
+        }
+    }
+
+    fn add(&mut self, data: &[u8]) {
+        let prev_hash = self.blocks.last().unwrap().hash.clone();
+        self.blocks.push(Block::new(data, &prev_hash));
+    }
+}
+
+fn main() {
+    let mut chain = Blockchain::new();
+    chain.add(b"hello world");
+    for block in chain.blocks {
+        println!("{:?}", block);
+    }
+}
